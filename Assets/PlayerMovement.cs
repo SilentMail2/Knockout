f6da6cf509277839b9c3705aa55ddf32d85b02e5
LiamFosdike <liamfosdike@gmail.com>
2018-05-26 13:33:45 +0930

    Simplified the script, added a simple jump mechanic
    Simplified the script so I don't have to go if (player == "#") for every character, just change the axisbutton string variable for each character

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -21,7 +21,22 @@ public class PlayerMovement : MonoBehaviour {
 	public float punchDir;
 	public float knockbackSpeed;
 	public float knockbackDir;
+	public float JumpSpeed;
 	public GameObject Weapon;
+	public bool canjump;
+	public float jump;
+
+	//Controlls
+	public string jumpButton;
+	public string horizontalButton;
+	public string FireButton;
+
+
+
+
+	public float jumpTime;
+	public float jumpTimer;
+	public float jumpCount;
 	// Use this for initialization
 	void Start () {
 		
@@ -31,19 +46,33 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 		if (playerNo == "1") 
 		{
-			Vector2 Movement = new Vector2 (Input.GetAxis ("Horizontal") * moveSpeed,0);
-
-			if (Input.GetAxis ("Horizontal")< 0) {
+			Vector2 Movement = new Vector2 (Input.GetAxis (horizontalButton) * moveSpeed, JumpSpeed*jump);
+			if (Input.GetButtonDown (jumpButton) && canjump)
+			{
+				jump = 1;
+				//canjump = false;
+			}
+			if (jumpTimer > 0 && jump > 0) {
+				jumpTimer = jumpTimer - jumpCount + Time.deltaTime;
+				jump = jump - 0.0001f;
+			}
+			if (jumpTimer <= 0) {
+				jump = 0;
+			}
+			if (jump == 0) {
+				jumpTimer = jumpTime + Time.deltaTime;
+			}
+			if (Input.GetAxis (horizontalButton)< 0) {
 				Vector3 newScale = obj.transform.localScale;
 				newScale.x = -1;
 				obj.transform.localScale = newScale;
 			}
-			if (Input.GetAxis ("Horizontal")> 0) {
+			if (Input.GetAxis (horizontalButton)> 0) {
 				Vector3 newScale = obj.transform.localScale;
 				newScale.x = 1;
 				obj.transform.localScale = newScale;
 			}
-			if (Input.GetAxis ("Fire1") > 0) {
+			if (Input.GetAxis (FireButton) > 0) {
 				if (!hasSwing) {
 					punch.SetActive (true);
 				}
@@ -57,62 +86,23 @@ public class PlayerMovement : MonoBehaviour {
 					swing.SetActive (false);
 				}
 			}
-			if (Input.GetAxis ("Fire1") <= 0) 
+
+		
+
+			if (Input.GetAxis (FireButton) <= 0) 
 			{
 				punchTimer = punchTime + Time.deltaTime;
 				swing.SetActive (false);
 
 				punch.SetActive (false);
 			}
-			if (Input.GetButtonUp ("Fire1")) 
+			if (Input.GetButtonUp (FireButton)) 
 			{
 				hasSwing = false;
 			}
 			rb2d.velocity = Movement;
 		}
-		if (playerNo == "2") 
-		{
-			Vector2 Movement = new Vector2 (Input.GetAxis ("Horizontal2") * moveSpeed,0);
 
-			if (Input.GetAxis ("Horizontal2") < 0) {
-				Vector3 newScale = obj.transform.localScale;
-				newScale.x = -1;
-				obj.transform.localScale = newScale;
-			}
-			if (Input.GetAxis ("Horizontal2") > 0) {
-				Vector3 newScale = obj.transform.localScale;
-				newScale.x = 1;
-				obj.transform.localScale = newScale;
-			}
-			if (Input.GetAxis ("Fire2") > 0) {
-				if (!hasSwing) {
-					punch.SetActive (true);
-				}
-				if (hasSwing) {
-					swing.SetActive (true);
-				}
-				punchTimer = punchTimer - punchCount + Time.deltaTime;
-				if (punchTimer <= 0) {
-					punch.SetActive (false);
-					hasSwing = false;
-					swing.SetActive (false);
-				}
-			}
-			if (Input.GetAxis ("Fire2") <= 0) 
-			{
-				punchTimer = punchTime + Time.deltaTime;
-				swing.SetActive (false);
-
-				punch.SetActive (false);
-			}
-			if (Input.GetButtonUp ("Fire2")) 
-			{
-				hasSwing = false;
-			}
-
-
-			rb2d.velocity = Movement;
-		}
 		if (hp <= 0) 
 		{
 			rb2d.AddForce (new Vector2 (knockbackDir * knockbackSpeed, 0));
