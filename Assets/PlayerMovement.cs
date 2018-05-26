c792d5d03f1c4bba50aebe1307f6af5c4237f821
LiamFosdike <liamfosdike@gmail.com>
2018-05-26 18:56:55 +0930

    edited level for further testing and started a score system

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed;
-	public string playerNo;
+	public int playerNo;
 	public Rigidbody2D rb2d;
 	public GameObject obj;
 	public GameObject punch;
@@ -25,6 +25,8 @@ public class PlayerMovement : MonoBehaviour {
 	public GameObject Weapon;
 	public bool canjump;
 	public float jump;
+	//scoresystem
+	public ScoreKeeper scoreSystem;
 
 	//Controlls
 	public string jumpButton;
@@ -32,42 +34,60 @@ public class PlayerMovement : MonoBehaviour {
 	public string FireButton;
 
 
-
+	public float gravity;
+	public float gravIncrease;
 
 	public float jumpTime;
 	public float jumpTimer;
 	public float jumpCount;
+
+	public LayerMask groundLayer;
+	public bool IsGrounded ()
+	{
+		Vector2 position = transform.position;
+		Vector2 direction = Vector2.down;
+		float distance = 2.0f;
+
+		RaycastHit2D hit = Physics2D.Raycast (position, direction, distance, groundLayer);
+
+		if (hit.collider != null) {
+			return true;
+		} 
+
+		return false;
+	}
 	// Use this for initialization
 	void Start () {
-		
+		Time.timeScale = 0.8f;
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
-		if (playerNo == "1") 
-		{
-			Vector2 Movement = new Vector2 (Input.GetAxis (horizontalButton) * moveSpeed, JumpSpeed*jump);
-			if (Input.GetButtonDown (jumpButton) && canjump)
-			{
+		if (hp > 0) {
+			Vector2 Movement = new Vector2 (Input.GetAxis (horizontalButton) * moveSpeed, JumpSpeed * jump - gravity);
+			if (Input.GetButtonDown (jumpButton) && IsGrounded ()) {
 				jump = 1;
-				//canjump = false;
+				gravity = 10;
+				
 			}
-			if (jumpTimer > 0 && jump > 0) {
-				jumpTimer = jumpTimer - jumpCount + Time.deltaTime;
-				jump = jump - 0.0001f;
+			if (/*jumpTimer > 0 &&*/ jump > 0) {
+				//jumpTimer = jumpTimer - jumpCount + Time.deltaTime;
+				jump = jump - gravIncrease;
+				//gravity = gravity + gravIncrease;
 			}
-			if (jumpTimer <= 0) {
-				jump = 0;
-			}
+			//if (jumpTimer <= 0) {
+			//jump = 0;
+			//}
 			if (jump == 0) {
 				jumpTimer = jumpTime + Time.deltaTime;
 			}
-			if (Input.GetAxis (horizontalButton)< 0) {
+			if (Input.GetAxis (horizontalButton) < 0) {
 				Vector3 newScale = obj.transform.localScale;
 				newScale.x = -1;
 				obj.transform.localScale = newScale;
 			}
-			if (Input.GetAxis (horizontalButton)> 0) {
+			if (Input.GetAxis (horizontalButton) > 0) {
 				Vector3 newScale = obj.transform.localScale;
 				newScale.x = 1;
 				obj.transform.localScale = newScale;
@@ -89,30 +109,45 @@ public class PlayerMovement : MonoBehaviour {
 
 		
 
-			if (Input.GetAxis (FireButton) <= 0) 
-			{
+			if (Input.GetAxis (FireButton) <= 0) {
 				punchTimer = punchTime + Time.deltaTime;
 				swing.SetActive (false);
 
 				punch.SetActive (false);
 			}
-			if (Input.GetButtonUp (FireButton)) 
-			{
+			if (Input.GetButtonUp (FireButton)) {
 				hasSwing = false;
 			}
 			rb2d.velocity = Movement;
-		}
 
-		if (hp <= 0) 
-		{
+
+
+			selfLocation = this.transform.position.x;
+			if (hasSwing) {
+				Weapon.SetActive (true);
+			}
+			if (!hasSwing) {
+				Weapon.SetActive (false);
+			}
+		}
+		if (hp <= 0) {
 			rb2d.AddForce (new Vector2 (knockbackDir * knockbackSpeed, 0));
 		}
-		selfLocation = this.transform.position.x;
-		if (hasSwing) {
-			Weapon.SetActive (true);
+	}
+	public void Death ()
+	{
+		scoreSystem.dead++;
+		if (scoreSystem.dead == 1) {
+			scoreSystem.points [playerNo] += 5;
 		}
-		if (!hasSwing) {
-			Weapon.SetActive (false);
+		if (scoreSystem.dead == 2) {
+			scoreSystem.points [playerNo] += 10;
+		}
+		if (scoreSystem.dead == 3) {
+			scoreSystem.points [playerNo] += 15;
+		}
+		if (scoreSystem.dead == 4) {
+			scoreSystem.points [playerNo] += 20;
 		}
 	}
 	void OnTriggerEnter2D (Collider2D other)
---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreKeeper : MonoBehaviour {
+	public GameObject[] playerList;
+	public float[] points;
+	public int dead;
+	// Use this for initialization
+	void Start () {
+		playerList = GameObject.FindGameObjectsWithTag ("Player");
+		//point list size = playerlistsize;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
